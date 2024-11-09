dd643a176db900acfc12fe6baca0aa26aa32f2e1
topmbr <centerm9911@gmail.com>
2024-11-09 11:11:32 +0200

    hello
    add hello

---
+++ Krypto/Krypto/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Krypto.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
